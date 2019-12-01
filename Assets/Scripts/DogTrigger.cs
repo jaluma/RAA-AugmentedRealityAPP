99d047572667479bff0b5f02bcbf052662f5d960
Javier Martínez <javiermartinezalvarez98@gmail.com>
2019-12-01 16:19:23 +0100

    Add new mechanics (TODO: fix spawn hearts)

---
+++ Assets/Scripts/DogTrigger.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogTrigger : MonoBehaviour
+{
+    private Animator _animator;
+    private float _time;
+    private int _lastState;
+
+    // Use this for initialization
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _time = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var an = _animator.GetCurrentAnimatorStateInfo(0);
+
+        if (_lastState == 0 || !an.shortNameHash.Equals(_lastState)) {
+            _time = 0;
+            _animator.SetBool("Idle", false);
+        }
+        _lastState = an.shortNameHash;
+
+        if (an.loop)
+        {
+            _time++;
+        }
+
+        var delay = an.length * 40 <= _time;
+
+        if (delay) {
+            _animator.SetBool("Idle", true);
+            _time = 0;
+        }
+
+        if (!delay && (an.IsName("SitDown") || an.IsName("IdleSit")))
+        {
+            _animator.SetBool("Sit", true);
+            _animator.SetBool("Idle", false);
+        }
+
+        if (an.IsName("Idle1")) {
+            _animator.SetBool("Sit", false);
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnAroundObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnAroundObject : MonoBehaviour
+{
+    public GameObject Prefab1;
+    public float Distance;
+    public int AmountOfObjectsToSpawn;
+
+    public void Spawn()
+    {
+        //var r = GetComponent<SphereCollider>().radius;
+        for (var i = 0; i < AmountOfObjectsToSpawn; i++)
+        {
+            Vector3 randomPos;
+            do
+            {
+                randomPos = Random.insideUnitSphere * Distance;
+                randomPos.y = 0;
+            } while (Physics.CheckSphere(randomPos, 3));
+
+            var position = transform.position + randomPos;
+            var o = Instantiate(Prefab1, position, Quaternion.identity).gameObject;
+            o.transform.position = randomPos;
+            o.transform.parent = transform;
+            Destroy(o, 3f);
+        }
+    }
+       
+}
